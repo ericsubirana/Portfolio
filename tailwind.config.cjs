891c6e15c21ed7578de652ab71366e9ed4825372
ericsubirana <ericsubirana4444@gmail.com>
2023-03-09 20:01:06 +0100

    paginaCasiFinalitzada

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,56 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{html,astro,js}'],
 	theme: {
-		extend: {},
+		extend: {
+			transitionProperty:{
+				'appBar': 'transform 0.6s ease-out'
+			},
+			backgroundImage:{
+				'blueback':"url('/images/backgroundCLIP.png')",
+			},
+			fontFamily:{
+				'robot':'Roboto Condensed',
+				'poppins':'Poppins',
+				'lemon':['LEMON']
+			},
+			keyframes:{
+				typing: {
+					'0%': { width: 0 },
+					'100%': { width: 490 }
+				},
+				blink_caret:{
+					'0%, 100%': {'border-color':' transparent'},
+					'50%' : {'border-color':' #ffb74d'},
+				},
+				back:{
+					'100%':{
+						'background-position':'2000px 0'
+					},
+				},
+				translate:{
+					'0%':{
+						'transform':'translateY(50%)'
+					},
+					'100%':{
+						'transform':' translateY(0)'
+					}
+				},
+				opacity:{
+					'0%':{
+						'opacity':'0'
+					},
+					'100%':{
+						'opacity':'1'
+					}
+				}
+			},
+			animation: {
+				'typing-A':'typing 3s steps(30, end), blink_caret .5s step-end infinite',
+				'back-A':'back 20s linear infinite',
+				'about-A':'translate 1s ease-in-out, opacity 1s',
+			},
+		},
 	},
 	plugins: [],
 }
