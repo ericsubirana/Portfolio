dc239e7aba54e9a977130d76eec060c06478e22d
Èric <113594121+ericsubirana@users.noreply.github.com>
2023-05-09 18:24:02 +0200

    responsive

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,25 @@
 module.exports = {
 	content: ['./src/**/*.{html,astro,js}'],
 	theme: {
+		screens: {
+			'smm' : {'max': '639px'}, 
+			// => @media (min-width: 20px) { ... }
+
+			'sm': '640px',
+			// => @media (min-width: 640px) { ... }
+	  
+			'md': '768px',
+			// => @media (min-width: 768px) { ... }
+	  
+			'lg': '1024px',
+			// => @media (min-width: 1024px) { ... }
+	  
+			'xl': '1280px',
+			// => @media (min-width: 1280px) { ... }
+	  
+			'2xl': '1536px',
+			// => @media (min-width: 1536px) { ... }
+		},
 		extend: {
 			backgroundImage: {
 				'hero': "url('/images/back5.png')"
